f979f780d12dde72e65a01044100ead059d63a16
pixeled <pixeled@mac.com>
2025-09-04 21:48:22 -0700

    push

--- SampleApp/app/src/main/java/com/example/findmyshade_example/DemoActivity.java
+++ SampleApp/app/src/main/java/com/example/findmyshade_example/DemoActivity.java
@@ -1,19 +1,26 @@
 package com.example.findmyshade_example;
 
-import android.content.ComponentName;
-import android.content.Intent;
+import com.ringoai.findmyshade.FaceCapture;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class DemoActivity extends AppCompatActivity {
+    private String kLicenseKey = "736BDFC8-0D70-4C44-81B2-49E5114D64FA";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.demo_activity_layout);
+        Error error = FaceCapture.setLicenseKey(getApplicationContext(), kLicenseKey);
+
+        if (error != null) {
+            Toast.makeText(this, error.toString(), Toast.LENGTH_SHORT).show();
+            return;
+        }
 
         Button launchButton = findViewById(R.id.run_sdk);
         launchButton.setOnClickListener(new View.OnClickListener() {
@@ -25,7 +32,6 @@ public class DemoActivity extends AppCompatActivity {
     }
 
     protected void launchSDK() {
-        Intent intent = new Intent("com.ringoai.findmyshade.SDKActivity");
-        startActivity(intent);
+        FaceCapture.FindMyShade(this);
     }
 }
